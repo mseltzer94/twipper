b1df8c72fe84ec1504ec821edce89006ebcb063d
Michael M. Seltzer <michael@theseltzers.com>
2014-11-22 12:54:16 -0500

    rails app created with navbar

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Twipper::Application.config.secret_token = 'df1494af27e1c7a99787ff6f06aaf38238ef33cabf5c9ac5a6d012a9e6c34c55652bb15171c765a8c1f7e773e6a0c301b52165da1f1582befeef5320002377c3'
